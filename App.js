76e021031266023d707dab2018c390462e75e70d
GaetanDupont <gaetan.dupont@live.fr>
2021-02-09 01:53:44 +0100

    :rocket: Add Film Card

--- App.js
+++ App.js
@@ -4,22 +4,21 @@ import SearchScreen from "./src/screens/SearchScreen";
 import {View, Image, StyleSheet} from "react-native";
 import Section from "./src/components/Section";
 import Category from "./src/components/Category";
+import FilmCards from "./src/components/FilmCards";
 
 export default function App() {
   return (
-      <View >
-        <Section />
-        <View style={styles.button}>
-        <Category/>
-        </View>
+      <View style = {styles.background}>
+        <FilmCards/>
       </View>
 
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-        paddingVertical: 12,
-        paddingHorizontal: 12,
-  },
+  background: {
+    flex:1,
+    backgroundColor: '#F4F4F4'
+  }
+
 })
---
+++ src/components/FilmCards.js
@@ -0,0 +1,99 @@
+// Components/FilmItem.js
+
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+
+class FilmCards extends React.Component {
+  render() {
+    return (
+      <View style={styles.main_container}>
+          
+        <Image
+          style={styles.image}
+          source={require('../../assets/images/starwars.jpg')}
+        />
+        
+        <View style={styles.content_container}>
+          <View style={styles.header_container}>
+            <Text style={styles.title_text}>Nom du film</Text>
+          </View>
+          <View style={styles.description_container}>
+            <Text style={styles.date_text}>1995</Text>
+          </View>
+          <View style={styles.director_container}>
+            <Text style={styles.director_text}>Nom réalisateur</Text>
+          </View>
+        </View>
+        <View style={styles.vote_container}>
+            <Text style={styles.vote_text}>9</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+  main_container: {
+    marginTop: 90,
+    marginHorizontal: 10,
+    flex: 0.2, // A changer possiblement si problèmes de taille
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    shadowOffset:{  width: 0,  height: 4,  },
+    shadowColor: '#B00020',
+    shadowOpacity: 0.7,
+  },
+
+  image: {
+    flex: 2,
+    resizeMode: 'contain',
+  },
+
+  content_container: {
+    justifyContent: 'space-around',
+    flex: 3,
+    margin: 10
+  },
+
+  header_container: {
+
+  },
+
+  title_text: {
+    color: '#B5A90F',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  date_container: {
+
+  },
+
+  date_text: {
+    color: '#B5A90F',
+    fontSize: 15,
+  },
+
+  director_container: {
+
+  },
+  director_text: {
+    color: '#B5A90F',
+    fontSize: 15,
+  },
+
+  vote_container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+  },
+
+  vote_text: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#B00020'
+  },
+})
+
+export default FilmCards
