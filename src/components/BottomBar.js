6045f8c19c8c9e145c640c5f8e3a1c789b765b52
HugoLeroyHL <hugo.leroy@hetic.net>
2021-02-11 11:34:50 +0100

    :wrench: fix bottombar bug

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -12,120 +12,56 @@ export default class BottomNavigator extends Component {
     render() {
         return (
             <View style={{
-                flex: 1,
-                flexDirection: 'column',
-                backgroundColor: 'grey'
-
+                  position: 'absolute',
+                  backgroundColor: 'white',
+                  border: 2,
+                  radius: 3,
+                  shadowOpacity: 0.3,
+                  shadowRadius: 3,
+                  shadowOffset: {
+                  height: 3, width: 3
+                  },
+                  x: 0,
+                  y: 0,
+                  style: { marginVertical: 5 },
+                  bottom: 0,
+                  width: '100%',
+                  height: 70,
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                  paddingVertical: 10,
+                  paddingHorizontal: 25
             }}>
-                <View style={{
-
-                    position: 'absolute',
-                    backgroundColor: 'white',
-                    border: 2,
-                    radius: 3,
-                    shadowOpacity: 0.3,
-                    shadowRadius: 3,
-                    shadowOffset: {
-
-                        height: 3, width: 3
-                    },
-                    x: 0,
-                    y: 0,
-                    style: { marginVertical: 5 },
-                    bottom: 0,
-                    width: '100%',
-                    height: 70,
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    paddingVertical: 10,
-                    paddingHorizontal: 25
-
-
-                }}>
-
-                    <View style={{
-
-
-                        flexDirection: 'column', alignItems: 'center', justifyContent: 'center'
-                    }}>
-                        <TouchableOpacity onPress={() => { Alert.alert('go sur la page home') }}>
-                            <Image
-
-                                style={{ width: 30, height: 30 }}
-
-                                source={{ uri: 'http://pluspng.com/img-png/home-icon-png-home-house-icon-image-202-512.png' }}
-
-                                onPress={()=>{Alert.alert("")}}
-                            >
-
-                            </Image>
-
-                        </TouchableOpacity>
-                        <Text style={{justifyContent:'center',alignItems:'center'}}>Home</Text>
-                    </View>
-                    
-                    <View style={{
-                        flexDirection: 'column', alignItems: 'center',justifyContent:'center',marginStart:30
-                    }}>
-
-                        <TouchableOpacity
-                            onPress={() => { Alert.alert("go sur la page recherche") }}
+                  <View style={{
+                  flexDirection: 'column', alignItems: 'center', justifyContent: 'center'
+                  }}>
+                  <TouchableOpacity onPress={() => { Alert.alert('go sur la page home') }}>
+                        <Image
+                              style={{ width: 30, height: 30 }}
+                              source={{ uri: 'http://pluspng.com/img-png/home-icon-png-home-house-icon-image-202-512.png' }}
+                              onPress={()=>{Alert.alert("")}}
                         >
-                            <Image
-                                style={{  width: 30, height: 30 }}
-                                source={{ uri: 'http://simpleicon.com/wp-content/uploads/active-search.png' }}
-                                onPress={() => { Alert.alert("") }}
-                            />
-                       
-                        </TouchableOpacity>
-                        <Text style={{justifyContent:'center',alignItems:'center' }}>search </Text>
-                    </View>
-                    {/* </View> */}
-                </View>
+                        </Image>
+                  </TouchableOpacity>
+                        <Text style={{justifyContent:'center',alignItems:'center'}}>Home</Text>
+                        </View>
+                        <View style={{
+                        flexDirection: 'column', alignItems: 'center',justifyContent:'center',marginStart:30
+                        }}>
+                  <TouchableOpacity
+                        onPress={() => { Alert.alert("go sur la page recherche") }}
+                  >
+                        <Image
+                              style={{  width: 30, height: 30 }}
+                              source={{ uri: 'http://simpleicon.com/wp-content/uploads/active-search.png' }}
+                              onPress={() => { Alert.alert("") }}
+                        />
+                  </TouchableOpacity>
+                  <Text style={{justifyContent:'center',alignItems:'center' }}>search </Text>
+                  </View>
             </View>
         );
     }
 
     
 }
-
-
-const styles = StyleSheet.create({
-
-    MainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'blue'
-    },
-    button: {
-        width: 60,
-        height: 60,
-        alignItems: 'center',
-        justifyContent: 'center',
-        shadowColor: 'grey',
-        shadowOpacity: 0.1,
-        shadowOffset: { x: 2, y: 0 },
-        shadowRadius: 2,
-        borderRadius: 30,
-        position: 'absolute',
-        bottom: 20,
-        right: 0,
-        top: 5,
-        left: 5,
-        shadowOpacity: 5.0,
-
-    },
-    actionBtn: {
-
-        backgroundColor: '#1E90FF',
-        textShadowOffset: { width: 5, height: 5 },
-        textShadowRadius: 10,
-        borderWidth: 2,
-        borderColor: '#fff'
-
-
-    }
-
-
-});
